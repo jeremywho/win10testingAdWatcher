3959e8c62dd09b4b4e7d94f3b1d521cb969a848b
jeremy <jeremy@weebu.com>
2019-02-04 13:38:03 -0800

    initial

---
+++ TestingBluetoothNativeWin10/BaseViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using TestingBluetoothNativeWin10.Annotations;
+
+namespace TestingBluetoothNativeWin10
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool AssignProperty<TProperty>(ref TProperty field, TProperty value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(field, value))
+                return false;
+
+            field = value;
+
+            // ReSharper disable once ExplicitCallerInfoArgument
+            OnPropertyChanged(propertyName);
+
+            return true;
+        }
+    }
+
+    public class Command : ICommand
+    {
+        private readonly Action _action;
+        private readonly bool _canExecute;
+        public Command(Action action, bool canExecute = true)
+        {
+            _action = action;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+    }
+}
---
+++ TestingBluetoothNativeWin10/MainWindowViewModel.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.Advertisement;
+
+namespace TestingBluetoothNativeWin10
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        public ObservableCollection<DeviceViewModel> Devices { get; } = new ObservableCollection<DeviceViewModel>();
+        private readonly BluetoothLEAdvertisementWatcher _watcher;
+       
+
+        public MainWindowViewModel()
+        {
+            _watcher = new BluetoothLEAdvertisementWatcher {ScanningMode = BluetoothLEScanningMode.Active};
+            _watcher.Received += OnAdvertisementReceived;
+            _watcher.Stopped += OnAdvertisementWatcherStopped;
+
+            _watcher.Start();
+        }
+
+        private void OnAdvertisementWatcherStopped(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementWatcherStoppedEventArgs args)
+        {
+            Debug.WriteLine($"{DateTime.Now} [{sender.Status}] [{sender.GetHashCode()}] stopped called");
+        }
+
+        private void OnAdvertisementReceived(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementReceivedEventArgs eventArgs)
+        {
+            //System.Diagnostics.Debug.WriteLine($"Got OnAdvertisementReceived");
+            // We can obtain various information about the advertisement we just received by accessing 
+            // the properties of the EventArgs class
+            if (string.IsNullOrEmpty(eventArgs.Advertisement.LocalName)) return;
+            // The timestamp of the event
+            DateTimeOffset timestamp = eventArgs.Timestamp;
+
+            // The type of advertisement
+            BluetoothLEAdvertisementType advertisementType = eventArgs.AdvertisementType;
+
+            // The received signal strength indicator (RSSI)
+            Int16 rssi = eventArgs.RawSignalStrengthInDBm;
+
+            // The local name of the advertising device contained within the payload, if any
+            string localName = eventArgs.Advertisement.LocalName;
+
+            // Check if there are any manufacturer-specific sections.
+            // If there is, print the raw data of the first manufacturer section (if there are multiple).
+            //string manufacturerDataString = "";
+            //var manufacturerSections = eventArgs.Advertisement.ManufacturerData;
+            //if (manufacturerSections.Count > 0)
+            //{
+            //    // Only print the first one of the list
+            //    var manufacturerData = manufacturerSections[0];
+            //    var data = new byte[manufacturerData.Data.Length];
+            //    using (var reader = DataReader.FromBuffer(manufacturerData.Data))
+            //    {
+            //        reader.ReadBytes(data);
+            //    }
+            //    // Print the company ID + the raw data in hex format
+            //    manufacturerDataString = string.Format("0x{0}: {1}",
+            //        manufacturerData.CompanyId.ToString("X"),
+            //        BitConverter.ToString(data));
+            //}
+
+            Debug.WriteLine($"[{DateTime.Now}] [{timestamp}] [{localName}] [{rssi}] [{advertisementType}]");
+
+            if (Devices.Any(d => d.BluetoothAddress == eventArgs.BluetoothAddress))
+                return;
+
+            var device = new DeviceViewModel(eventArgs.Advertisement.LocalName, eventArgs.BluetoothAddress);
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                Devices.Add(device);
+            });
+        }
+    }
+
+    public class DeviceViewModel : BaseViewModel
+    {
+        public DeviceViewModel(string name, ulong addr, string id = "")
+        {
+            Name = name;
+            Id = id;
+            BluetoothAddress = addr;
+            Status = BluetoothConnectionStatus.Disconnected.ToString();
+        }
+
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set => AssignProperty(ref _name, value);
+        }
+
+        private string _id;
+        public string Id
+        {
+            get => _id;
+            set => AssignProperty(ref _id, value);
+        }
+
+        private string _status;
+        public string Status
+        {
+            get => _status;
+            set => AssignProperty(ref _status, value);
+        }
+
+        private ulong _bluetoothAddress;
+        public ulong BluetoothAddress
+        {
+            get => _bluetoothAddress;
+            set => AssignProperty(ref _bluetoothAddress, value);
+        }
+
+        public string BtAddressDisplay => BluetoothAddress.ToString();
+    }
+}
